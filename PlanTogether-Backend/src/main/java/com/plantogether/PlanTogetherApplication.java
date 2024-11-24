c1e9c3538b4bc82d51d0870da0f757dc334fb65b
lydts <lydia.tsami@gmail.com>
2024-11-24 21:25:27 +0200

    Initial Commit

---
+++ PlanTogether-Backend/src/main/java/com/plantogether/PlanTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.plantogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanTogetherApplication.class, args);
+    }
+
+}
---
+++ PlanTogether-Backend/src/test/java/com/plantogether/PlanTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
